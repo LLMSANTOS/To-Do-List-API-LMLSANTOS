80016dbc2f9a2507827e1d5ed6d4bc915e38c1b8
LLMSANTOS <64105498+LLMSANTOS@users.noreply.github.com>
2023-06-17 06:10:50 +0000

    todo api LMLSANTOS v2.0

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 
 const TodoList = ()=> {
 
-    const [todo, setToDo] = useState({});
-    const [todoList, setToDoList] = useState([]);
+    const [todo, setTodo] = useState({});
+    const [todoList, setTodoList] = useState([]);
     const [username, setUsername] = useState("");
 
     useEffect(()=> {
@@ -12,30 +12,39 @@ const TodoList = ()=> {
 
     // function to add a username
     const createUsername = () => {
-        fetch ("https://assets.breatheco.de/apis/fake/todos/user"+username, {
-            method: 'POST',
-            headers: {'Content-type':'application/json'},
-            body: JSON.stringify([]),
-        }).then((response) => response.json())
-        .then(responseAsJson => {
-            console.log(responseAsJson);
-            getTodos();
-        }).catch((error)=> {
-            console.log(error);
-        });
+        if(username!==""){
+           
+            fetch ("https://assets.breatheco.de/apis/fake/todos/user/" + username, {
+                method: 'POST',
+                headers: {'Content-type':'application/json'},
+                body: JSON.stringify([]),
+            })
+            .then((response) => response.json())
+            .then(responseAsJson => {
+                console.log(responseAsJson);
+                getTodos();
+            }).catch((error)=> {
+                console.log(error);
+            });
+        }  
+        else {
+            alert("Please insert a username!");
+        }
     }
 
     // function to acquire todos
     const getTodos = () => {
         if(username !==""){
-            fetch("https://assets.breatheco.de/apis/fake/todos/user"+username, {
+            fetch("https://assets.breatheco.de/apis/fake/todos/user/" + username, {
                 method: 'GET',
                 headers: {'Content-type':'application/json'},
-            }).then((response) => response.json())
+            })
+            .then((response) => response.json())
             .then(responseAsJson => {
-                console.log(responseAsJson);
-                setToDo(responseAsJson);
-            }).catch((error)=> {
+               
+                setTodoList(responseAsJson);
+            })
+            .catch((error)=> {
                 console.log(error);
             });
         }
@@ -44,32 +53,61 @@ const TodoList = ()=> {
     // function to update todos
     const updateTask = () => {
         const todos = [...todoList, todo]
-        setToDoList(todos)
+        setTodoList(todos)
+
+       /*try { 
+            const response= await fetch ("https://assets.breatheco.de/apis/fake/todos/user/"+username, {
+                method: 'PUT',
+                headers: {'Content-type':'application/json'},
+                body: JSON.stringify(todos),
+            })
+            console.log(response)
+            const data = await response.json();
+            console.log(data)
+        } catch {
+            console.error("error on create user", error)
+        }*/
        
-        fetch ("https://assets.breatheco.de/apis/fake/todos/user"+username, {
+        fetch ("https://assets.breatheco.de/apis/fake/todos/user/"+username, {
             method: 'PUT',
             headers: {'Content-type':'application/json'},
-            body: JSON.stringify([todos]),
-        }).then((response) => response.json())
+            body: JSON.stringify(todos),
+        })
+        .then((response) => response.json())
+           
         .then(responseAsJson => {
             console.log(responseAsJson);
         }).catch((error)=> {
             console.log(error);
         });
     }
+   
+ 
+    const deleteTask = (deleteItem) => {
 
-    // function to delete a task
-    const handleDelete = (deleteItem) => {
-		
-		const newTask = todoList.filter((todoItem) => todoItem !== deleteItem);
-		setToDoList(newTask);
-	}
-  
+        const newTask = todoList.filter((todoItem) => todoItem.label !== deleteItem.label);
+		setTodoList(newTask);
+
+        fetch ("https://assets.breatheco.de/apis/fake/todos/user/"+username, {
+            method: 'PUT',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(newTask),
+        })
+        .then((response) => response.json())
+        .then(responseAsJson => {
+            console.log(responseAsJson);
+        }).catch((error)=> {
+            console.log(error);
+        });
+
+    }
+     
     return (
         <div className="container">	
             <h2>todos</h2>
             <div className="user">
-                <input type="text"
+                <input 
+                    type="text"
                     value={username}
                     onChange={(e) => {
                     setUsername(e.target.value)
@@ -80,31 +118,33 @@ const TodoList = ()=> {
                         }
                     }}
                     placeholder="Insert your name..."
-                    />	
+                />	
             </div>
             
             <div className="notepad">				
                 <div className="inputArea">
-                    <input type="text"
+                    <input
+                        type="text"
                         value={todo.label}
                         onChange={(e) => 
-                        setToDo({label: e.target.value, done: false})}
+                        setTodo({label: e.target.value, done: false})}
                         onKeyDown={(e) => {
                             if(e.key === "Enter"){
                                  updateTask();
-                                 setToDo("");
+                                 setTodo({label:"", done: false});
                             }
                         }}
-                        placeholder="What needs to be done?"/>		
+                        placeholder="What needs to be done?"
+                    />		
                 </div>
             <div className="text-center">
                 <ul className="listItems">
-                    {todoList.length !== 0 &&
+                    {todoList.length >= 0 &&
                     todoList.map((todoItem) => (
                     <li key={todoItem}>
                     <div className="listItemContainer">
                         <span className="todoItem">{todoItem.label}</span>
-                        <i className="fas fa-times" onClick={() => handleDelete(todoItem)}></i>   						
+                        <i className="fas fa-times" onClick={() => {deleteTask(todoItem)}}></i>   						
                     </div>
                     </li>
                     ))}
